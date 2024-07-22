808f771c20c2e5007a222c011813ff3fb06b1ca1
Joanna Chen <joannachenchencom@gmail.com>
2024-07-23 00:48:02 +0800

    feat: WebSocket group setting

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -9,7 +9,35 @@ public class ChatHub: Hub
         _logger = logger;
     }
 
-    public async Task SendMessage(Message message)
+    public async Task JoinGroup(string groupId)
+    {
+        try
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
+            _logger.LogTrace($"JoinGroup Success: {groupId}");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"JoinGroup Error: {ex.Message}");
+            _logger.LogError($"JoinGroup Error: {ex.Message}");
+        }
+    }
+
+    public async Task LeaveGroup(string groupId)
+    {
+        try
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId);
+            _logger.LogTrace($"LeaveGroup Success: {groupId}");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"LeaveGroup Error: {ex.Message}");
+            _logger.LogError($"LeaveGroup Error: {ex.Message}");
+        }
+    }
+
+    public async Task SendGlobalMessage(Message message)
     {
         // 記錄收到的消息
         _logger.LogInformation($"Received message: {JsonSerializer.Serialize(message)}");
@@ -24,4 +52,21 @@ public class ChatHub: Hub
             _logger.LogError($"SendMessage Error: {ex.Message}");
         }
     }
+
+    public async Task SendGroupMessage(string groupId, GroupMessage message)
+    {
+        // 記錄收到的消息
+        _logger.LogInformation($"Received group message: {JsonSerializer.Serialize(message)}");
+        try
+        {
+            string messageJson = JsonSerializer.Serialize(message);
+            await Clients.Group(groupId).SendAsync("GroupMessage", messageJson);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"SendGroupMessage Error: {ex.Message}");
+            _logger.LogError($"SendGroupMessage Error: {ex.Message}");
+        }
+    }
+
 }
---
+++ Models/GroupMessage.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+public class GroupMessage : Message 
+{
+    [JsonPropertyName("groupName")]
+    public string GroupName {get; set;}
+    [JsonPropertyName("groupId")]
+    public string GroupId {get; set;}
+
+    public GroupMessage(string user, string traceId, string content, string createdTime, string groupName, string groupId) : base(user, traceId, content, createdTime)
+    {
+        GroupName = groupName;
+        GroupId = groupId;
+    }
+}
